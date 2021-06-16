592a8651eaed906176324dd95edcb7d01b448211
Sayantan Basu <sayantanbasu@live.in>
2021-06-16 06:53:18 -0400

    counter fn works but does not recognise backspace

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,22 +1,27 @@
+function charCounter(string) {
+  counter = 0;
+  for (let i = 0; i < string.length; i++) {
+    counter++;
+  }
+  return counter;
+}
+
 $(document).ready(function () {
   // --- our code goes here ---
   console.log("page hass loaded & ready to be manipulated!");
-  //fn to count string
-  function counter(string) {
-    counter = 0;
-    for (let i = 0; i < string.length; i++) {
-      counter++;
-    }
-    return counter;
-  }
-
-  let input = document.getElementById("tweet-text");
-  let count = document.getElementsByClassName("counter");
-
+  let input = document.querySelector('#tweet-text');;
+  let count = document.querySelector('.counter');
+  //let tweetBtn = document.getElementByClassName("btn"); 
   input.addEventListener("keydown", () => {
     let humanInput = input.value;
     console.log(humanInput);
-    let output = counter(humanInput);
-    count.innerHTML = output;
+    let output = charCounter(humanInput);
+   if(output > 140){
+    count.innerHTML = 140 - output ;
+     count.style.color = 'red';
+     $("#btn").attr("disabled");
+   } else {
+     count.innerHTML = 140 - output ;
+   }
   });
 }, false);
