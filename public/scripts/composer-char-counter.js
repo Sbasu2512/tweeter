a94262bb16281235ee5c04ebf541fbc1a5a8040b
Sayantan Basu <sayantanbasu@live.in>
2021-06-16 05:51:59 -0400

    added fn to count char and added event listener

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // --- our code goes here ---
   console.log("page hass loaded & ready to be manipulated!");
+  //fn to count string
+  function counter(string) {
+    counter = 0;
+    for (let i = 0; i < string.length; i++) {
+      counter++;
+    }
+    return counter;
+  }
+  
+  let input = document.getElementById("tweet-text");
+  let count = document.getElementsByClassName("counter");
+
+  document.addEventListener("keydown", () => {
+    let humanInput = input.value;
+    let output = counter(humanInput);
+    count.innerHTML = output;
+  });
 });
