967608963a519ed8102cdf53f551420dd08f1d29
Sayantan Basu <sayantanbasu@live.in>
2021-06-22 16:20:13 -0400

    fixed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,41 +5,39 @@
  */
 let tweetData = [
   {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
     },
-    "created_at": 1461116232227
+    created_at: 1461116232227,
   },
   {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
     },
-    "created_at": 1461113959088
-  }
-]
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
 
 $(document).ready(function () {
-  
-  function renderTweets(arr)  {
+  function renderTweets(arr) {
     $(".tweets").empty();
     for (let element of arr) {
       const tweet = createTweetElement(element);
-      $(".tweets").append(tweet);
+      $(".tweets").prepend(tweet);
     }
-  };
+  }
 
   const createTweetElement = (tweetObject) => {
-    // console.log("tweet obj is: ", tweetObject["content"].text);
     const tweetTemplate = `<div class='tweet-container'>
   <div class='useravatar'>
     <p><img class='reincarnation' src ='${tweetObject["user"].avatars}'/></p>
@@ -61,58 +59,54 @@ $(document).ready(function () {
     return tweetTemplate;
   };
 
-function loadTweets() {
-  renderTweets([]);
-  $.ajax({
-    method: 'GET',
-    url: '/tweets'
-  })
-    .done((result) => {
-      renderTweets(result);
-    })
-    .fail(err => console.log(err));
-};
-
-
-
-$('#tweet-form').submit(function(event) {
-  event.preventDefault();
-  // first validate for empty/long tweets
-  const $tweetBox = $(this).find('#tweet-text');
-  const $counter = $(this).find('.counter');
- const emptyMsg = $(this).siblings('.empty-tweet-err');
-   const longMsg = $(this).siblings('.long-tweet-err');
-  if ($tweetBox.val() === "") {
-    $(longMsg).slideUp(10);
-    $(emptyMsg).slideDown(200);
-  } else if ($tweetBox.val().length > 140) {
-    $(emptyMsg).slideUp(10);
-    $(longMsg).slideDown(200);
-  } else {
-    const tweetSerialized = $tweetBox.serialize();
+  function loadTweets() {
+    renderTweets([]);
     $.ajax({
-      method: 'POST',
-      url: '/tweets',
-      data: tweetSerialized
+      method: "GET",
+      url: "/tweets",
     })
-      .done(result => {
-        loadTweets();
-        $tweetBox.val('');
-        $counter.val(140);
-        $(emptyMsg).slideUp(100);
-        $(longMsg).slideUp(100);
+      .done((result) => {
+        renderTweets(result);
       })
-      .fail(err => console.log(err));
+      .fail((err) => console.log(err));
   }
-});
 
-loadTweets();
+  $("#tweet-form").submit(function (event) {
+    event.preventDefault();
+    // first validate for empty/long tweets
+    const $tweetBox = $(this).find("#tweet-text");
+    const $counter = $(this).find(".counter");
+    const emptyMsg = $(this).siblings(".empty-tweet-err");
+    const longMsg = $(this).siblings(".long-tweet-err");
+    if ($tweetBox.val() === "") {
+      $(longMsg).slideUp(10);
+      $(emptyMsg).slideDown(200);
+    } else if ($tweetBox.val().length > 140) {
+      $(emptyMsg).slideUp(10);
+      $(longMsg).slideDown(200);
+    } else {
+      const tweetSerialized = $tweetBox.serialize();
+      $.ajax({
+        method: "POST",
+        url: "/tweets",
+        data: tweetSerialized,
+      })
+        .done((result) => {
+          loadTweets();
+          $tweetBox.val("");
+          $counter.val(140);
+          $(emptyMsg).slideUp(100);
+          $(longMsg).slideUp(100);
+        })
+        .fail((err) => console.log(err));
+    }
+  });
 
-const escape = function (str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
+  loadTweets();
 
-
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 });
